d37c460dbf1544a4b386e2985ea9ed841d7c3e19
alexfortin <fortin.alex14@gmail.com>
2022-10-28 18:49:52 -0500

    added voter routes

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const app = express()
 const PORT = process.env.PORT || 3001
 const db = require("./db/connection")
 const apiRoutes = require("./routes/apiRoutes")
